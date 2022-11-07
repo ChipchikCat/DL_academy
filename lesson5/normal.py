04f6dccedc155d58b97ed3f9bf8220f47b32d9d4
Andrew <kuba.z.chipchik@gmail.com>
2022-11-07 21:11:56 +0300

    commit5

--- lesson5/normal.py
+++ lesson5/normal.py
@@ -1,8 +1,13 @@
+__author__ = 'Соловьев Андрей Сергеевич.'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+
+# TODO:
+
+import re
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -20,6 +25,9 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+pattern = "([a-z]+)"
+print("List1 =", re.findall(pattern, line))
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,6 +36,8 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+# TODO:
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +55,45 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+pattern2 = "([a-z]{2})([A-Z]+)([A-Z]{2})"
+
+lst1 = [el for el in re.findall(pattern2, line_2)]
+lst2 = []
+
+for el in lst1:
+    lst2.append(el[1])
+
+print("List2 =", lst2)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+# TODO:
+
+import random
+
+with open("random_numbers.txt", "a", encoding="UTF-8") as f:
+    for i in range(0, 2500):
+        f.write(str(random.randint(0, 9)))
+
+max_list = []
+
+with open("random_numbers.txt", "r", encoding="UTF-8") as f:
+    list = []
+    last_symb = ""
+    for string in f:
+        for symbol in string:
+            if symbol == last_symb:
+                list.append(symbol)
+            else:
+                if len(max_list) < len(list):
+                    max_list = list
+                list = []
+                list.append(symbol)
+            last_symb = symbol
+
+print(max_list)
---
+++ lesson8/loto.py
@@ -0,0 +1,181 @@
+"""
+== Лото ==
+
+Правила игры в лото.
+
+Игра ведется с помощью специальных карточек, на которых отмечены числа, 
+и фишек (бочонков) с цифрами.
+
+Количество бочонков — 90 штук (с цифрами от 1 до 90).
+
+Каждая карточка содержит 3 строки по 9 клеток. В каждой строке по 5 случайных цифр, 
+расположенных по возрастанию. Все цифры в карточке уникальны. Пример карточки:
+
+--------------------------
+    9 43 62          74 90
+ 2    27    75 78    82
+   41 56 63     76      86 
+--------------------------
+
+В игре 2 игрока: пользователь и компьютер. Каждому в начале выдается 
+случайная карточка. 
+
+Каждый ход выбирается один случайный бочонок и выводится на экран.
+Также выводятся карточка игрока и карточка компьютера.
+
+Пользователю предлагается зачеркнуть цифру на карточке или продолжить.
+Если игрок выбрал "зачеркнуть":
+	Если цифра есть на карточке - она зачеркивается и игра продолжается.
+	Если цифры на карточке нет - игрок проигрывает и игра завершается.
+Если игрок выбрал "продолжить":
+	Если цифра есть на карточке - игрок проигрывает и игра завершается.
+	Если цифры на карточке нет - игра продолжается.
+	
+Побеждает тот, кто первый закроет все числа на своей карточке.
+
+Пример одного хода:
+
+Новый бочонок: 70 (осталось 76)
+------ Ваша карточка -----
+ 6  7          49    57 58
+   14 26     -    78    85
+23 33    38    48    71   
+--------------------------
+-- Карточка компьютера ---
+ 7 11     - 14    87      
+      16 49    55 77    88    
+   15 20     -       76  -
+--------------------------
+Зачеркнуть цифру? (y/n)
+
+Подсказка: каждый следующий случайный бочонок из мешка удобно получать 
+с помощью функции-генератора.
+
+Подсказка: для работы с псевдослучайными числами удобно использовать 
+модуль random: http://docs.python.org/3/library/random.html
+
+"""
+
+# TODO:
+
+from random import randint
+
+class Player:
+    def __init__(self, name, player_type="computer"):
+        self.name = name
+        self.player_type = player_type
+        self.card = Card("{}".format(name))
+        self._number_of_win = 15
+
+
+    def check_barrel(self, barrel):
+        barrel_in_cart = False
+        barrel_line = None
+        barrel_column = None
+        if self.player_type == "human":
+            for line_num, line in enumerate(self.card.lines):
+                if barrel in line:
+                    barrel_in_cart = True
+                    barrel_line = line_num
+                    barrel_column = line.index(barrel)
+                    break
+            answer = input("Is barrel in your card? y/n/q")
+            if answer == "y" and barrel_in_cart:
+                self.cross_barrel(barrel_line, barrel_column)
+                self._number_of_win -= 1
+            elif (answer == "y" and not barrel_in_cart) or (answer == "n" and barrel_in_cart):
+                print("You are lose")
+            elif answer == "n" and not barrel_in_cart:
+                print("correct")
+            elif answer == "q":
+                print("You are lose")
+            else:
+                print("You are lose")
+        else:
+            for line_num, line in enumerate(self.card.lines):
+                if barrel in line:
+                    barrel_in_cart = True
+                    barrel_line = line_num
+                    barrel_column = line.index(barrel)
+                    break
+        if barrel_in_cart:
+            self.cross_barrel(barrel_line, barrel_column)
+            self._number_of_win -= 1
+
+    def cross_barrel(self, line, column):
+        self.card.lines[line][column] = "-X"
+
+    def chek_winner(self):
+        if self._number_of_win == 0:
+            print("{} are winner".format(self.name))
+            return False
+        else:
+            return True
+class Bag:
+    def __init__(self):
+        self.bag = list(range(1, 91))
+        print(self.bag)
+
+    def _calculate_random_position(self):
+        return randint(0, len(self.bag) - 1)
+    def get_barrel(self):
+        random_pos = self._calculate_random_position()
+        return str(self.bag.pop(random_pos))
+
+class Card:
+    def __init__(self, name):
+        self.name = name
+        self.lines = [
+            list("__" for j in range(0, 9)) for i in range(3)
+        ]
+        self._put_numbers()
+    def show_lines(self):
+        print("-" * 15, "{}".format(self.name), "-" * 15)
+        for i in self.lines:
+            print(i)
+        print("-" * 20, "-" * 20)
+
+    def _gen_numbers(self, stop_search=15, start=1, end=90):
+        stop = stop_search
+        my_numbers = []
+
+        while stop:
+            number = randint(start, end)
+            if number not in my_numbers:
+                my_numbers.append(number)
+                stop -= 1
+
+        return my_numbers
+
+    def _put_numbers(self):
+        numbers = self._gen_numbers()
+
+        otr1 = numbers[:5]
+        otr2 = numbers[5:10]
+        otr3 = numbers[10:15]
+        otrs = [otr1, otr2, otr3]
+        for stroka, otr in zip(self.lines, otrs):
+            sorted_otr = sorted(otr)
+            rand_position = self._gen_numbers(5, 0, 8)
+            rand_position = sorted(rand_position)
+            for num, pos in zip(sorted_otr, rand_position):
+                stroka[pos] = str(num)
+
+def game():
+    player = Player("Bob", "computer")
+
+
+    player2 = Player("Mike", "computer")
+
+    bag = Bag()
+    while len(bag.bag) > 0 and player.chek_winner() and player2.chek_winner():
+        print(len(bag.bag))
+        barrel = bag.get_barrel()
+        print(barrel)
+        player.card.show_lines()
+        player2.card.show_lines()
+        player.check_barrel(barrel)
+        player2.check_barrel(barrel)
+
+
+game()
